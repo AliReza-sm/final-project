6c27853dfe58ab4e8c0f5a853b9d76f49c7cfb47
alisa <ali.sargazi1373@gmail.com>
2025-06-12 17:21:18 +0330

    starting point of program

---
+++ src/main/java/ir/maktabsharif/homeserviceprovidersystem/HomeServiceProviderSystemApplication.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif.homeserviceprovidersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceProviderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceProviderSystemApplication.class, args);
+	}
+
+}
